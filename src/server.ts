cea98dd89678c07135da2ca102a7f92cd0b8a90c
souzannatha <souzanatha04@gmail.com>
2026-03-09 21:14:31 -0300

    chore: add  eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({
-  port: env.PORT,
-  host: '0.0.0.0'
-}).then(() => {
-  console.log('HTTP Server Runing!')
-})
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP Server Runing!')
+  })
