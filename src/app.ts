e1f19103fc1a4f2d334a44f661e05e5c8746cb06
souzannatha <souzanatha04@gmail.com>
2026-03-10 15:17:57 -0300

    chore: setup prisma and docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Nathã Souza Lopos',
+    email: 'souzanatha04@gmail.com',
+  },
+})
