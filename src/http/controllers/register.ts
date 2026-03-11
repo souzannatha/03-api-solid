2b220e42969555e64c32f9d1d3695383ed6517eb
souzannatha <souzanatha04@gmail.com>
2026-03-10 21:28:46 -0300

    feat: errors in use cases validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -23,7 +24,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (err) {
-    reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      reply.status(409).send({ message: err.message })
+    }
+    reply.status(500).send() //TODO: fix me
   }
 
   return reply.status(201).send()
