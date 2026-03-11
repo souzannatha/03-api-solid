3753efeb74e515ba80899368bf08aa5bea247d6b
souzannatha <souzanatha04@gmail.com>
2026-03-11 20:31:27 -0300

    fix: change name of route authenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { app } from '@/app'
 import { register } from './controllers/register'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes() {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
