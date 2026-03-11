b198f7ad964997b273bf26a97d4fdaa0207efa42
souzannatha <souzanatha04@gmail.com>
2026-03-11 20:46:50 -0300

    refactor: instances of authenticate and register tests

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,10 +1,18 @@
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, beforeEach } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { AuthenticateUseCase } from './authenticate'
 import { hash } from 'bcryptjs'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
+
 describe('Authenticate Use Case', () => {
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository
+    sut = new AuthenticateUseCase(usersRepository)
+  })
 
   it('should to be able to authenticate', async () => {
     const usersRepository = new InMemoryUsersRepository
