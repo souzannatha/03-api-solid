152e3840b99f681d03e2debc7d9358f1d1f53507
souzannatha <souzanatha04@gmail.com>
2026-03-22 17:46:58 -0300

    feat: add fwt in fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -14,9 +14,19 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
+    })
+
+    const token = await reply.jwtSign({}, {
+      sign: {
+        sub: user.id
+      }
+    })
+
+    return reply.status(200).send({
+      token
     })
   } catch (err) {
 
@@ -27,5 +37,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
     throw err
   }
 
-  return reply.status(200).send()
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { app } from '@/app'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes() {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  /**Authenticated Routes */
+  app.get('/me', profile)
 }
