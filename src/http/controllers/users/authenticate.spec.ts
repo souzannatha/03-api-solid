3bc3e762c761777bac46f356d2989ced94fb8d6f
souzannatha <souzanatha04@gmail.com>
2026-03-26 19:54:46 -0300

    feat: create test e2e for authenticate user.

--- src/http/controllers/users/authenticate.spec.ts
+++ src/http/controllers/users/authenticate.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 
 import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
 import { app } from '@/app'
+import { any } from 'zod'
 
 describe('Authenticate (e2e)', () => {
   beforeAll(async () => {
@@ -14,6 +15,23 @@ describe('Authenticate (e2e)', () => {
   })
 
   it('should be able to authenticate', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
 
+    const response = await request(app.server)
+      .post('/sessions')
+      .send({
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String)
+    })
   })
 })
