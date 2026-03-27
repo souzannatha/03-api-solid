3c808d7f91d7740e85e68ab1afa9fbd2b9cd7359
souzannatha <souzanatha04@gmail.com>
2026-03-27 20:28:26 -0300

    feat: add refresh token route and test.

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -25,9 +25,23 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
       }
     })
 
-    return reply.status(200).send({
-      token
+    const refreshToken = await reply.jwtSign({}, {
+      sign: {
+        sub: user.id,
+        expiresIn: '7d'
+      }
     })
+
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true, // HTTPs
+        sameSite: true,
+        httpOnly: true,
+      }).status(200).send({
+        token
+      })
   } catch (err) {
 
     if (err instanceof InvalidCredentialsError) {
