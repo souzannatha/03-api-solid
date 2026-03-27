f5cc1859818ba705bc4c87c39a7d8d38a6e39197
souzannatha <souzanatha04@gmail.com>
2026-03-26 21:36:50 -0300

    feat: create all test routes of check-ins

--- src/http/controllers/check-ins/create.ts
+++ src/http/controllers/check-ins/create.ts
@@ -1,30 +1,37 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
-import { makeCreateGymsUseCase } from '@/use-cases/factories/make-create-gyms-use-case'
 import { makeCheckInUseCase } from '@/use-cases/factories/make-check-in-use-case'
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
   const createCheckInParamsSchema = z.object({
-    gymId: z.coerce.string()
+    gymId: z.string().uuid(),
   })
 
   const createCheckInBodySchema = z.object({
-    latitude: z.number().refine(value => {
+    latitude: z.number().refine((value) => {
       return Math.abs(value) <= 90
     }),
-    longitude: z.number().refine(value => {
+    longitude: z.number().refine((value) => {
       return Math.abs(value) <= 180
     }),
-
   })
-  const { gymId } = createCheckInParamsSchema.parse(request.params)
-  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+  try {
+    const { gymId } = createCheckInParamsSchema.parse(request.params)
+    const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
 
-  const checkInUseCase = makeCheckInUseCase()
+    const checkInUseCase = makeCheckInUseCase()
 
-  await checkInUseCase.execute({
-    gymId, userId: request.user.sub, userLatitude: latitude, userLongitude: longitude
-  })
+    await checkInUseCase.execute({
+      gymId,
+      userId: request.user.sub,
+      userLatitude: latitude,
+      userLongitude: longitude,
+    })
 
-  return reply.status(201).send()
+    return reply.status(201).send()
+  } catch (err) {
+    console.error(err) // 👈 ISSO AQUI
+    throw err
+  }
+
 }
--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,16 +1,21 @@
 import { app } from "@/app";
-import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { create } from "./create";
 import { validate } from "./validate";
 import { history } from "./history";
 import { metrics } from "./metrics";
+import { verifyJWT } from '@/http/middlewares/verify-jwt';
 
 export async function checkInsRoutes() {
 
-  app.get('/check-ins/hisotry', history)
-  app.get('/check-ins/metrics', metrics)
+  app.get('/check-ins/history', { onRequest: [verifyJWT] }, history)
+  app.get('/check-ins/metrics', { onRequest: [verifyJWT] }, metrics)
 
-  app.post('/gyms/:gymId/check-ins', create)
+
+  app.post(
+    '/gyms/:gymId/check-ins',
+    { onRequest: [verifyJWT] },
+    create,
+  )
 
   app.patch('/check-ins/:checkInId/validate', validate)
 }
